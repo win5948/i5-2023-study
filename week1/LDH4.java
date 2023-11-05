6e36f0a30582f3da0cf2f25cd3b69978980b4c3f
leedahye426 <73814583+leedahye426@users.noreply.github.com>
2023-11-05 23:28:43 +0900

    LDH-소수만들기 (#22)

--- week1/LDH4.java
+++ week1/LDH4.java
@@ -1,21 +1,28 @@
-class Solution {
+import java.util.*;
+
+class Solution {   
     public int solution(int[] nums) {
         int answer = 0;
+        boolean[] isPrime = new boolean[3001];
+        Arrays.fill(isPrime, true);
+        
+        isPrime[0] = isPrime[1] = false;
+        
+        for(int i = 2; i <= 3000; i++) {
+            if(isPrime[i]) {
+                for(int j = i * i; j <= 3000; j+=i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
         int sum = 0;
-
-        for(int i = 0; i < nums.length - 2; i++) {
-            for(int j = i + 1; j < nums.length - 1; j++) {
-                for(int k = j + 1; k < nums.length ; k++) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                for(int k = j + 1; k < nums.length; k++) {
                     sum = nums[i] + nums[j] + nums[k];
-                    
-                    boolean isPrime = true;
-                    for(int l = 2; l < sum; l++) {
-                        if(sum % l == 0) { // 2부터 sum-1까지 약수가 존재하면 isPrime을 false로 변경하고 해당 반복문을 빠져나온다.
-                            isPrime = false;
-                            break;
-                        }
-                    }
-                    if(isPrime) answer++; // isPrime이 true라면 위의 if문에 걸리지 않은 것이므로 소수이다.
+
+                    if(isPrime[sum]) answer++; 
                 }
             }
         }
